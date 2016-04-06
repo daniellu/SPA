2fd51d3d6b8daa0ec67f33ddef4b0d7dd903f499
zlu <luzhanxun.daniel@gmail.com>
2016-04-05 22:20:29 -0700

    be able to move bar

--- CSPA/Scripts/app/ChartController.js
+++ CSPA/Scripts/app/ChartController.js
@@ -46,6 +46,54 @@
             series: seriesData
         });//end of chart
 
+        $('#graphContainer').highcharts().xAxis[0].setExtremes(
+				seriesData[0].data[0][0],
+				seriesData[0].data[50][0]
+			);
+
     };//end of generateChart
+
+    $('#button').click(function () {
+        var chart = $('#graphContainer').highcharts();
+
+        var currentExtreme = chart.xAxis[0].getExtremes();
+
+        var newRange = DecideNewTimeRange(currentExtreme.min, currentExtreme.max, JSON.parse($scope.Items[0].Data));
+
+        if (newRange != null) {
+            chart.xAxis[0].setExtremes(
+				newRange[0],
+				newRange[1]
+			);
+        }
+
+    });
+
+    function DecideNewTimeRange(currentStartTime, currentEndTime, allData) {
+        var resultRange = [];
+        var startIndex = null;
+        var endIndex = null;
+
+        for (var i = 0; i < allData.length; i++) {
+            if ((allData[i][0] <= currentStartTime && allData[i + 1][0] >= currentStartTime)) {
+                startIndex = i;
+            }
+            if ((allData[i][0] <= currentEndTime && allData[i + 1][0] >= currentEndTime)) {
+                endIndex = i;
+            }
+        }
+
+        if ((endIndex == allData.length - 1)) {
+            alert('On the extreme');
+            resultRange[0] = currentStartTime;
+            resultRange[1] = currentEndTime;
+        }
+        else {
+            resultRange[0] = allData[startIndex + 1][0];
+            resultRange[1] = allData[endIndex + 1][0];
+        }
+
+        return resultRange;
+    }//end of DecideNewTimeRange
 
 });
