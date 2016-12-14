738cb263533f11cb98aa29706f729bbab08c6c98
Daniel <luzhanxun.daniel@gmail.com>
2016-12-14 08:07:17 -0800

    Add Playground folder

---
+++ PlayGround/src/index.js
@@ -0,0 +1,27 @@
+require('./assets/css/default.css');//import the global css file
+
+import Vue from 'vue'
+import Links from './components/Links.vue'
+import Home from './components/Home.vue'
+
+import VueRouter from 'vue-router'
+import VueResource from 'vue-resource'
+Vue.use(VueResource)
+Vue.use(VueRouter)
+
+export var router = new VueRouter({
+  mode: 'history',
+  base: __dirname,
+  routes: [
+    { path: '/', component: Home},
+    { path: '/links', component: Links}
+  ]
+  }
+);
+
+
+new Vue({
+  router,
+  
+}).$mount('#app')
+
